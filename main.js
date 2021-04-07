c3975b33a63cb27ad5f973e0830136c291b0c3fb
David <raymondinf23@gmail.com>
2021-04-06 20:01:12 -0400

    Add dynamic image change with js

---
+++ main.js
@@ -0,0 +1,13 @@
+const imageElement = document.querySelector('.background-image');
+const images = ['./img/cinthia-aguilar-6KL5DRngGRM-unsplash.jpg',
+                './img/kid.jpg',
+                './img/rowan-heuvel-U6t80TWJ1DM-unsplash.jpg'];
+let times = 0;
+
+setInterval(changeImage, 4500)
+
+function changeImage(){
+    imageElement.src = images[times];
+    times = (times + 1) % images.length;
+    console.log(times);
+}
